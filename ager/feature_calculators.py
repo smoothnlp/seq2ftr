f1f63fd30ddb10d5c78752c0ae7af9a09601746d
zhangruinan <ruinan.zhang@icloud.com>
2019-03-19 12:11:06 +0800

    add more calculators

--- ager/feature_calculators.py
+++ ager/feature_calculators.py
@@ -14,6 +14,22 @@ def listify_type(x):
         return x
     return listify
 
+##########################
+## Supporting Funcitons ##
+##########################
+
+def _shift(x:list,n:int):
+    return x[n:]+x[:n]
+
+def _sort(x:list):
+    x_copy = x.copy()
+    x_copy.sort()
+    return x_copy
+global x_sorted
+
+#########################
+## Feature Calculators ##
+#########################
 @set_property("name","mean","stypes",[0,1])
 @listify_type
 def _mean(x:list):
@@ -24,10 +40,29 @@ def _mean(x:list):
 def _max(x:list):
     return max(x)
 
+@set_property("name","freq_of_max","stypes",[1])
+def _freq_of_max(x:list):
+    max_x = _max(x)
+    return len([xi for xi in x if xi >=max_x])
+
 @set_property("name","min","stypes",[0,1])
 @listify_type
 def _min(x:list):
     return min(x)
+
+@set_property("name","freq_of_min","stypes",[1])
+def _freq_of_min(x:list):
+    min_x = _min(x)
+    return len([xi for xi in x if xi<=min_x])
+
+@set_property("name",'median',"stypes",[1])
+def _median(x:list):
+    x_sorted = _sort(x)
+    x_len = len(x_sorted)
+    if x_len%2==1:
+        return x_sorted[x_len//2]
+    else:
+        return x_sorted[x_len//2]*0.5 + x_sorted[x_len//2-1]*0.5
 
 @set_property("name","variance","stypes",[0,1])
 @listify_type
@@ -40,3 +75,12 @@ def _var(x:list):
 @listify_type
 def _uniqueCount(x:list):
     return len(set(x))
+
+@set_property("name","length","stypes",[0,1,2])
+@listify_type
+def _len(x:list):
+    return len(x)
+
+@set_property("name","num_duplicates","stypes",[0,1,2])
+def _num_duplicates(x:list):
+    return _len(x) - _uniqueCount(x)
