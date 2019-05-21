e67eac3f85b6818100f778f03327cdccca03d237
yjun1989 <yjun1989@gmail.com>
2019-05-21 17:54:22 +0800

    add long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
+import os
+rootdir = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(rootdir, 'readme.rst')).read()
 
 setuptools.setup(
     name="seq2ftr",
-    version="0.1.5",
+    version="0.1.7",
     author="Ruinan(Victor) Zhang， YinJun(YJ)",
     author_email="ruinan.zhang@icloud.com, yjun1989@gmail.com",
     description="A Nice and Convenient Feature Engineering Tool on Sequential Data",
-    long_description='readme.rst',
+    long_description=long_description,
     long_description_content_type="",
     url="https://github.com/smoothnlp/seq2ftr",
     packages=setuptools.find_packages(),
